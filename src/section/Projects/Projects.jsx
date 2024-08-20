7226ab4e257884088ad220d369e4b4cfa53edca4
Hirushan Rajapaksha <136220511+HiruRajapaksha@users.noreply.github.com>
2024-08-21 03:15:04 +0530

    Project Updated

--- src/section/Projects/Projects.jsx
+++ src/section/Projects/Projects.jsx
@@ -4,6 +4,7 @@ import trip from '../../assets/trip.png';
 import medical from '../../assets/medical.png';
 import tv from '../../assets/tv.png';
 import ProjectCard from '../../common/ProjectCard';
+import online_shopping from '../../assets/online-shopping.png';
 
 function Projects() {
   return (
@@ -21,6 +22,10 @@ function Projects() {
             <ProjectCard src={tv} link="https://github.com/HiruRajapaksha/Tv-Show-App.git"
             h3="TV-Shows"
             p="TV-Show App"/>
+
+            <ProjectCard src={online_shopping} link="https://github.com/HiruRajapaksha/E-Commerce.git"
+            h3="StyleHaven"
+            p="E-commerce Website"/>
         </div>
     </section>
   );
